f87c75436a7681d09cfdf649a9fd65d34b20a279
Joshua Mann <mann.josh@yahoo.com>
2021-10-05 12:49:17 -0400

    Added logical comparisons

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -14,6 +14,15 @@ namespace WiredBrainCoffeeSurveys.Reports
             Console.WriteLine($"Response Percentage: {responseRate}");
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
             Console.WriteLine($"Overall Score: {overallScore}");
+
+            // Logical Comparisons
+            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
         }
     }
 }
