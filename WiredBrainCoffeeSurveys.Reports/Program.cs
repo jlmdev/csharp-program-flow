7e88c7aa462de3236ce6651ae220019853c7dcf2
Joshua Mann <mann.josh@yahoo.com>
2021-10-05 12:30:08 -0400

    Refactored to combine declaration and assignment of  responseRate and unansweredCount

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,11 +6,8 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            double responseRate;
-            double unansweredCount;
-
-            responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             
             Console.WriteLine($"Response Percentage: {responseRate}");
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
